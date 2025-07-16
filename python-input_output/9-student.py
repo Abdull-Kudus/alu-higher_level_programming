994743047043a83ddb45bf2a7612ffe0ef051860
Abdull-Kudus <a.zakariam@alustudent.com>
2025-07-16 01:36:13 -0700

    Add

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,29 +1,17 @@
-
 #!/usr/bin/python3
-"""
-This program is to define a Student in a class
-"""
+"""class_to_json"""
 
 
-class Student():
-    """
-    Class of a student
+class Student:
+    """Contains student data
     """
 
     def __init__(self, first_name, last_name, age):
-        """
-        Constructor of a student
-        Args:
-          - first_name: str
-          - last_name: str
-          - age: int
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Returns the dict representation of an instance
-        """
-        return (self.__dict__)
+        """class_to_json"""
+
+        return self.__dict__
