9edac949963fcbdcb264e8d08c5ac03800e605de
Abdull-Kudus <a.zakariam@alustudent.com>
2025-07-16 01:33:49 -0700

    Add

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""
-This program helps to write in a file and if it doesn't exist, it creates the file.
-"""
+"""Are you documented ?"""
 
 
 def write_file(filename="", text=""):
-        """
-        Write in a file, if doesn't exists it creates the file
-        Args:
-        - filename: string
-        - text: string
-        """
-
-        with open(filename, mode="w", encoding="utf-8") as _file:
-            _file.write(text)
-
-            return (len(text))
+    """I document you"""
+    with open(filename, 'w+') as f:
+        return f.write(text)
